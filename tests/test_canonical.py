7b3d44cf9e853016d6b90cfb0db2ac2ab48b0896
jburg3ss <nexo.cheek614@passinbox.com>
2025-10-26 17:11:06 +0000

    add unit test for cannonical header parser

--- tests/test_canonical.py
+++ tests/test_canonical.py
@@ -1,7 +1,28 @@
 # tests/test_canonical.py
 
 import pytest
-from header_analyzer.core.parsers.canonical import parse_headers, canonicalize_header
 
-def test_parse_simple_header():
-    pass
+from header_analyzer.core.parsers.canonical import canonicalize_header
+
+
+@pytest.mark.parametrize(
+    "header,expected",
+    [
+        ("from", "From"),
+        ("content-type", "Content-Type"),
+        ("content-transfer-encoding", "Content-Transfer-Encoding"),
+        ("message-id", "Message-ID"),
+        ("x-mailer", "X-Mailer"),
+        ("resent-message-id", "Resent-Message-ID"),
+        ("authentication-results", "Authentication-Results"),
+        ("received-spf", "Received-SPF"),
+    ],
+)
+def test_known_headers(header, expected):
+    """
+
+    :param header: param expected:
+    :param expected:
+
+    """
+    assert canonicalize_header(header) == expected
